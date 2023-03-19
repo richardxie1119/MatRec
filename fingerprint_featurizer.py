3b233bf6738d70b5086ef07a29555908fce82b51
richardxie1119 <xieyuxuan1996@gmail.com>
2023-03-19 16:14:17 -0500

    Add files via upload

--- fingerprint_featurizer.py
+++ fingerprint_featurizer.py
@@ -15,26 +15,30 @@ def main():
 	vect_list = []
 
 	feat = SiteStatsFingerprint.from_preset("CrystalNNFingerprint_ops")
+	fail_names = []
+	names_all = list(iglob('./POSITIONS_2016/struct_comp_DB/DB/*/*'))
 
-	for names in iglob('./POSITIONS_2016/struct_comp_DB/DB/*/*'):
-		print(names)
-	#try:
-		# read into pymatgen structure
-		strc = Structure.from_file(names)
+	for names in tqdm(names_all):
 
-		# featurize the pymatgen structure
-		feat_i = feat.featurize(strc)
-		vect_list.append(feat_i)
-		name_list.append(names.replace('/POSITIONS_2016/struct_comp_DB/DB/','').split('/')[-1])
+		try:
+			# read into pymatgen structure
+			strc = Structure.from_file(names)
 
-		#except: 
-		#	print('Failure:', names)
+			# featurize the pymatgen structure
+			feat_i = feat.featurize(strc)
+			vect_list.append(feat_i)
+			name_list.append(names.replace('/POSITIONS_2016/struct_comp_DB/DB/','').split('/')[-1])
+
+		except: 
+			#print('Failure:', names)
+			fail_names.append(names)
 
 	vec_dic = {name_list[i]: vect_list[i] for i in range(len(name_list))}
 
 	with open('featurized_vectors.pickle', 'wb') as f:
 	    pickle.dump(vec_dic, f)
-
+	fail_names = pd.DataFrame(fail_names)
+	fail_names.to_csv('fail_names.csv')
 
 if __name__ == "__main__":
 	main()
